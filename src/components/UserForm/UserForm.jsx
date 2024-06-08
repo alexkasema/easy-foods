c0a78ab349f12b30e037c96ceb60d659cb71ea6e
AlexKasema <lexxmulwa@gmail.com>
2024-06-08 21:02:40 +0300

    Implemented edit/update functionality for each user for the admin. The admin can make another user an admin as well and also edit their information.

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import AddressInputs from "../AddressInputs/AddressInputs";
+import { useProfile } from "../UseProfile";
 
 const UserForm = ({ user, onSave }) => {
   const [userName, setUserName] = useState(user?.name || "");
@@ -11,6 +12,7 @@ const UserForm = ({ user, onSave }) => {
   const [city, setCity] = useState(user?.city || "");
   const [country, setCountry] = useState(user?.country || "");
   const [admin, setAdmin] = useState(user?.admin || false);
+  const { data: loggedInUserData } = useProfile();
 
   function handleAddressChange(propName, value) {
     if (propName === "phone") setPhone(value);
@@ -64,6 +66,24 @@ const UserForm = ({ user, onSave }) => {
           addressProps={{ phone, streetAddress, postalCode, city, country }}
           setAddressProp={handleAddressChange}
         />
+        {loggedInUserData.admin && (
+          <div>
+            <label
+              className="p-2 inline-flex items-center gap-2 mb-2"
+              htmlFor="adminCb"
+            >
+              <input
+                id="adminCb"
+                type="checkbox"
+                className=""
+                value={"1"}
+                checked={admin}
+                onChange={(ev) => setAdmin(ev.target.checked)}
+              />
+              <span>Admin</span>
+            </label>
+          </div>
+        )}
         <button className="btn-primary" type="submit">
           Save
         </button>
